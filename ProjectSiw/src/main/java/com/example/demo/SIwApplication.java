172477cb10176ef36aff8ecec77682da93192233
Edoardo-Putti <edo.putti@stud.uniroma3.it>
2019-06-05 09:20:40 +0200

    push della base del progetto

---
+++ ProjectSiw/src/main/java/com/example/demo/SIwApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+@SpringBootApplication
+public class SIwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SIwApplication.class, args);
+	}
+
+
+}
